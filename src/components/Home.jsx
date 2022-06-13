d0b60bde5fb3f3f2cb6f924c7d4eefe011e0c311
ishsta <ishstaglass@yahoo.com>
2022-06-13 01:23:44 -0500

    fixed view work button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import {Link} from 'react-scroll'
 
 const Home = () => {
+
+
   return (
     <div name='home' className='w-full h-screen bg-[#7d6f5c]'>
         
@@ -11,11 +14,11 @@ const Home = () => {
             <h2 className='text-4xl sm:text-7xl font-bold text-[#DFD8CF]'> The Developer</h2>
             <p className='text-[#DFD8CF] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
             <div>
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#A6967F]'>View Work 
+                <Link to="work" smooth={true} duration={500}><button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#A6967F]'>View Work 
                 <span className='group-hover:rotate-90 duration-300'>
                 <HiArrowNarrowRight className='ml-3' />
                 </span>
-                </button>
+                </button></Link>
             </div>
         </div>
     </div>
